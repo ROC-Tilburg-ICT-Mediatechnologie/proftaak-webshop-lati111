e84bdc0595a0b7882f900b6645a575f0c7b42f83
lati111 <jvzelst111@gmail.com>
2022-11-15 12:29:46 +0100

    Fixed cart add repeating

--- views/cart.php
+++ views/cart.php
@@ -1,19 +1,30 @@
 <!doctype html>
 <html lang="nl">
+
 <head>
     <meta charset="utf-8">
     <title>Boodschappenwagentje</title>
 </head>
-<body>
-<h1>De inhoud van uw winkelwagentje</h1>
-<ul>
-    <?php
+
+<body onload="init()">
+    <h1>De inhoud van uw winkelwagentje</h1>
+    <ul>
+        <?php
         foreach ($items as $item) {
-        $price = ($item->quantity * $item->price) / 100;
+            $price = ($item->quantity * $item->price) / 100;
             echo "<li>$item->quantity x $item->name | € $price</li>";
         }
-    ?>
-    <a href="index.php">Verder winkelen</a>
-</ul>
+        ?>
+        <a href="index.php">Verder winkelen</a>
+    </ul>
 </body>
+
+<script>
+    function init() {
+        if (document.URL.indexOf("m=add") >= 0) {
+            window.history.pushState([], '', "index.php?c=cart&m=show")
+        }
+    }
+</script>
+
 </html>

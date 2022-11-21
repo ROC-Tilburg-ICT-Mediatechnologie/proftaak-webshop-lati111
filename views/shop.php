79fdcba58700409744b69a7c8d64667b038c159f
lati111 <jvzelst111@gmail.com>
2022-11-21 10:25:17 +0100

    Added message to shop view

--- views/shop.php
+++ views/shop.php
@@ -6,6 +6,11 @@
     <title>De shop</title>
 
     <style>
+        #message {
+            font-size: 2em;
+            font-weight: bold;
+        }
+
         .product img {
             max-width: 10em;
             max-height: 8em;
@@ -17,13 +22,17 @@
     </style>
 </head>
 
-<body>
+<body onload="init()">
     <h1>De shop</h1>
     <div>
         <a href="index.php?c=cart&m=show">Bekijk winkelwagen</a>
     </div>
     <table id="product_list">
         <?php
+        if (isset($message)) {
+            echo "<div id='message'>{$message}</div>";
+        }
+
         foreach ($products as $product) {
             $price = number_format($product->price / 100, 2);
             $price = str_replace(",", "-", $price);
@@ -35,11 +44,22 @@
                 "<td><span class='product_name'>{$product->name}</span></td>" .
                 "<td><span class='product_description'>{$product->description}</span></td>" .
                 "<td><span class='product_price'>€ {$price}</span></td>" .
-                "<td><a href='index.php?c=cart&m=add&id={$product->idproduct}'>Voeg toe aan winkelmandje</a></td>".
+                "<td><a href='index.php?c=cart&m=add&id={$product->idproduct}'>Voeg toe aan winkelmandje</a></td>" .
                 "</tr>";
         }
         ?>
     </table>
 </body>
+
+<script>
+    function init() {
+        if (document.URL.indexOf("m=showMessage") >= 0) {
+            setTimeout(() => {
+                document.getElementById("message").remove();
+                window.history.pushState([], '', "index.php")
+            }, 2000);
+        }
+    }
+</script>
 
 </html>
